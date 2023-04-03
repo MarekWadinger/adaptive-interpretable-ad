862c901c71723bea98c2cf4062b0c35b4444282b
marekwadinger <marekwadinger5506@gmail.com>
2023-04-03 13:03:33 +0200

    ADD: plot grid

--- functions/plot.py
+++ functions/plot.py
@@ -1,4 +1,6 @@
 # IMPORTS
+import textwrap
+
 from datetime import timedelta
 
 import pandas as pd
@@ -110,29 +112,10 @@ def plot_limits(
         fig.write_image(f"plot/{file_name}_anomalies.pdf")
 
     if (ser_high is not None) and (ser_low is not None):
-        fig.add_trace(go.Scatter(
-            x=ser.index, y=([1] if ser_high.max(skipna=True) < 1
-                            else [ser_high.max(skipna=True)])*len(ser),
-            line_color='rgba(100,100,100, 0)',
-            name='Threshold', legendgroup='thresh', showlegend=False,
-            line_width=0.7
-        ))
-
-        fig.add_trace(go.Scatter(
-            x=ser_high.index, y=ser_high,
-            line_color='rgba(100,0,0,0.25)',
-            fillcolor='rgba(100,0,0, 0.1)', fill="tonexty",
-            name='Threshold', legendgroup='thresh', showlegend=True,
-            line_width=0.7
-        ))
-
-        fig.add_trace(go.Scatter(
-            x=ser_low.index, y=ser_low,
-            line_color='rgba(100,0,0,0.25)',
-            fillcolor='rgba(100,0,0, 0.1)', fill="tozeroy",
-            name='Threshold', legendgroup='thresh', showlegend=False,
-            line_width=0.7
-        ))
+        yaxis_range = [ser.min(), ser.max()]
+        add_thresholds(fig, ser_high, ser_low,
+                       row=None, col=None,
+                       yaxis_range=yaxis_range)
 
         if save:
             fig.write_image(f"plot/{file_name}_thresh.pdf")
@@ -163,6 +146,7 @@ def plot_limits_3d(
         anomalies: pd.Series,
         ser_high: pd.Series | None = None,
         ser_low: pd.Series | None = None,
+        signal_anomalies: pd.Series | None = None,
         y: str | None = None,
         z: str | None = None,
         file_name: str | None = None,
@@ -210,34 +194,29 @@ def plot_limits_3d(
         line_width=0.7
     ), row=4, col=1)
 
+    yaxis_range = [df.iloc[:, col1].min(), df.iloc[:, col1].max()]
+
     # THRESHOLD
     thresh_high = ser_high.apply(lambda x: x[col1][col1])
     thresh_low = ser_low.apply(lambda x: x[col1][col1])
-    fig.add_trace(go.Scatter(
-        x=thresh_high.index, y=(
-            [thresh_high.max(skipna=True)])*len(thresh_high),
-        line_color='rgba(100,100,100, 0)',
-        name='Threshold', legendgroup='thresh', showlegend=False,
-        line_width=0.7
-    ), row=4, col=1)
+    add_thresholds(fig, thresh_high, thresh_low,
+                   row=4, col=1,
+                   yaxis_range=yaxis_range)
 
-    fig.add_trace(go.Scatter(
-        x=thresh_high.index, y=thresh_high,
-        line_color='rgba(100,0,0,0.25)',
-        fillcolor='rgba(100,0,0, 0.1)', fill="tonexty",
-        name='Threshold', legendgroup='thresh', showlegend=False,
-        line_width=0.7
-    ), row=4, col=1)
+    # ANOMALIES
+    if signal_anomalies is not None:
+        df_anomalies = df[signal_anomalies.apply(lambda x: x[y]) == 1]
+        fig.add_trace(go.Scatter(
+            x=df_anomalies.index,
+            y=df_anomalies.iloc[:, col1],
+            connectgaps=False,
+            name='Anomalies',
+            line_color='rgb(150,0,0)', showlegend=True,
+            line_width=0.7, mode='markers', marker_size=2
+        ), row=4, col=1)
 
-    fig.add_trace(go.Scatter(
-        x=thresh_low.index, y=thresh_low,
-        line_color='rgba(100,0,0,0.25)',
-        fillcolor='rgba(100,0,0, 0.1)', fill="tozeroy",
-        name='Threshold', legendgroup='thresh', showlegend=False,
-        line_width=0.7
-    ), row=4, col=1)
     fig.layout['yaxis'].update(
-        dict(range=[thresh_low.min(), thresh_high.max()]))
+        dict(range=yaxis_range))
 
     # THIRD
     fig.add_trace(go.Scatter(
@@ -248,34 +227,29 @@ def plot_limits_3d(
         line_width=0.7
     ), row=4, col=2)
 
+    yaxis_range = [df.iloc[:, col2].min(), df.iloc[:, col2].max()]
+
     # THRESHOLD
     thresh_high = ser_high.apply(lambda x: x[col2][col2])
     thresh_low = ser_low.apply(lambda x: x[col2][col2])
-    fig.add_trace(go.Scatter(
-        x=thresh_high.index, y=(
-            [thresh_high.max(skipna=True)])*len(thresh_high),
-        line_color='rgba(100,100,100, 0)',
-        name='Threshold', legendgroup='thresh', showlegend=False,
-        line_width=0.7
-    ), row=4, col=2)
+    add_thresholds(fig, thresh_high, thresh_low,
+                   row=4, col=2,
+                   yaxis_range=yaxis_range)
 
-    fig.add_trace(go.Scatter(
-        x=thresh_high.index, y=thresh_high,
-        line_color='rgba(100,0,0,0.25)',
-        fillcolor='rgba(100,0,0, 0.1)', fill="tonexty",
-        name='Threshold', legendgroup='thresh', showlegend=False,
-        line_width=0.7
-    ), row=4, col=2)
+    # ANOMALIES
+    if signal_anomalies is not None:
+        df_anomalies = df[signal_anomalies.apply(lambda x: x[z]) == 1]
+        fig.add_trace(go.Scatter(
+            x=df_anomalies.index,
+            y=df_anomalies.iloc[:, col2],
+            connectgaps=False,
+            name='Anomalies',
+            line_color='rgb(150,0,0)', showlegend=True,
+            line_width=0.7, mode='markers', marker_size=2
+        ), row=4, col=2)
 
-    fig.add_trace(go.Scatter(
-        x=thresh_low.index, y=thresh_low,
-        line_color='rgba(100,0,0,0.25)',
-        fillcolor='rgba(100,0,0, 0.1)', fill="tozeroy",
-        name='Threshold', legendgroup='thresh', showlegend=False,
-        line_width=0.7
-    ), row=4, col=2)
     fig.layout['yaxis2'].update(
-        dict(range=[thresh_low.min(), thresh_high.max()]))
+        dict(range=yaxis_range))
 
     fig.update_layout(
         height=90*10,
@@ -286,6 +260,46 @@ def plot_limits_3d(
         fig.write_html(f"plot/{file_name}_multi_all.html")
 
     return fig
+
+
+def add_thresholds(
+        fig,
+        thresh_high,
+        thresh_low,
+        row: int,
+        col: int,
+        yaxis_range: list[int]):
+    fig.add_trace(go.Scatter(
+        x=thresh_high.index, y=(
+            [max(yaxis_range)])*len(thresh_high),
+        line_color='rgba(100,100,100, 0)',
+        name='Threshold', legendgroup='thresh', showlegend=False,
+        line_width=0.7
+    ), row=row, col=col)
+
+    fig.add_trace(go.Scatter(
+        x=thresh_high.index, y=thresh_high,
+        line_color='rgba(100,0,0,0.25)',
+        fillcolor='rgba(100,0,0, 0.1)', fill="tonexty",
+        name='Threshold', legendgroup='thresh', showlegend=False,
+        line_width=0.7
+    ), row=row, col=col)
+
+    fig.add_trace(go.Scatter(
+        x=thresh_low.index, y=(
+            [min(yaxis_range)])*len(thresh_low),
+        line_color='rgba(100,100,100, 0)',
+        name='Threshold', legendgroup='thresh', showlegend=False,
+        line_width=0.7
+    ), row=row, col=col)
+
+    fig.add_trace(go.Scatter(
+        x=thresh_low.index, y=thresh_low,
+        line_color='rgba(100,0,0,0.25)',
+        fillcolor='rgba(100,0,0, 0.1)', fill="tonexty",
+        name='Threshold', legendgroup='thresh', showlegend=False,
+        line_width=0.7
+    ), row=row, col=col)
 
 
 def plot_compare_anomalies(
@@ -346,6 +360,154 @@ def plot_compare_anomalies(
 
     if save:
         fig.write_html(f"plot/{file_name}_compare_anomalies.html")
+
+    fig.show()
+
+
+def plot_limits_grid(
+        df: pd.DataFrame,
+        anomalies: pd.Series,
+        ser_high: pd.Series | None = None,
+        ser_low: pd.Series | None = None,
+        file_name: str | None = None,
+        save: bool = True,
+        changepoints: pd.Series | None = None,
+        samplings: pd.Series | None = None,
+        **kwargs):
+
+    a = anomalies.astype(int).diff()
+    # Show dates of anomalous events
+    b = a[a == 1].resample('1d').sum()
+
+    n_rows = len(df.columns)
+    fig = make_subplots(rows=n_rows, cols=1,
+                        shared_xaxes=True,
+                        vertical_spacing=0.05)
+    fig.update_layout(FIG_LAYOUT.update(
+            dict(height=180*3,
+                 width=60*3,)))
+
+    for row, col_name in zip([2, 3, 4, 1], df.columns):
+
+        col = df.columns.get_loc(col_name)
+        ser = df[col_name]
+        ser_high_ = ser_high.apply(lambda x: x[col][col])
+        ser_low_ = ser_low.apply(lambda x: x[col][col])
+
+        text = "<br>".join(textwrap.wrap(f"{ser.name} [-]", 15))
+        fig.update_yaxes(
+            title_text=text, title_font_size=9, title_standoff=15,
+            row=row, col=1)
+
+        fig.add_trace(go.Scatter(
+            x=ser.index, y=ser,
+            connectgaps=False,
+            line_color='rgb(0,140,120)',
+            name="Signal", showlegend=True if row == 1 else False,
+            line_width=0.7
+        ), row=row, col=1)
+
+        fig.add_trace(go.Scatter(
+            x=ser[anomalies == 1].index,
+            y=ser[anomalies == 1],
+            connectgaps=False,
+            name='System Anomalies',
+            line_color='rgb(150,0,0)', showlegend=True if row == 1 else False,
+            line_width=0.7, mode='markers', marker_size=3
+        ), row=row, col=1)
+
+        if isinstance(changepoints, pd.Series):
+            fig.add_trace(go.Scatter(
+                x=ser[samplings == 1].index,
+                y=ser[samplings == 1],
+                connectgaps=False,
+                name='Sampling Anomalies',
+                line_color='rgb(0,0,150)',
+                showlegend=True if row == 1 else False,
+                line_width=0.7, mode='markers', marker_size=2
+            ), row=row, col=1)
+
+            a = samplings.astype(int).diff()
+            for x0, x1 in zip(a[a == 1].index, a[a == -1].index):
+                fig.add_vrect(
+                    x0=x0, x1=x1,
+                    line_color="blue", fillcolor="blue", opacity=0.25,
+                    layer="below", row=row, col=1)
+
+        if isinstance(changepoints, pd.Series):
+            c = changepoints.astype(int).diff()
+            fig.add_trace(go.Scatter(
+                x=ser[c == 1].index,
+                y=ser[c == 1],
+                connectgaps=False,
+                name='Changepoint',
+                line_color='rgb(255,255,0)',
+                showlegend=True if row == 1 else False,
+                line_width=0.7, mode='markers', marker_size=2
+            ), row=row, col=1)
+
+            for x0 in c[c == 1].index:
+                x1 = c.index[c.index.get_loc(x0)+1]
+                fig.add_vrect(x0=x0, x1=x1,
+                              line_color="yellow", fillcolor="yellow",
+                              opacity=0.75, layer="below", row=row, col=1)
+
+        if (ser_high_ is not None) and (ser_low_ is not None):
+            yaxis_range = [ser.min(), ser.max()]
+            add_thresholds(fig, ser_high_, ser_low_,
+                           row=row, col=1,
+                           yaxis_range=yaxis_range)
+
+        fig.update_yaxes(tickfont_size=9, row=row, col=1)
+
+    fig.update_xaxes(tickangle=60, tickfont_size=9, tickvals=b[b > 0].index,
+                     row=row, col=1)
+
+    fig.update_layout(FIG_LAYOUT.update(
+            dict(height=180*3,
+                 width=60*3,
+                 yaxis_title=ser.name,
+                 yaxis_range=[ser.min(),
+                              ser.max()],
+                 xaxis_tickangle=60,
+                 xaxis_tickfont_size=9,
+                 xaxis_tickvals=b[b > 0].index,
+                 )))
+
+    if save:
+        FIG_LAYOUT_SAVE = dict(
+            height=160*3,
+            width=90*3,
+
+            yaxis_title_standoff=0,
+            xaxis_tickfont_size=9,
+
+            font_family="Times New Roman",
+            font_size=9,
+
+            autosize=False,
+
+            margin=dict(l=0, r=0, t=0, b=0),
+            bargap=0,
+
+            legend=dict(
+                orientation="h",
+                yanchor="bottom",
+                y=1.02,
+                xanchor="right",
+                x=1,
+                font_size=9
+            )
+        )
+        fig.update_layout(FIG_LAYOUT_SAVE)
+        fig.write_image(f"plots/{file_name}_thresh.pdf")
+
+    fig.update_layout(
+        height=90*10,
+        width=120*10,
+    )
+    if save:
+        fig.write_html(f"plots/{file_name}_all.html")
 
     fig.show()
 
