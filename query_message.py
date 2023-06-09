ce1223153d1bdb234bc21b8cf1fa4d373494335e
marekwadinger <marekwadinger5506@gmail.com>
2023-06-09 16:47:13 +0200

    FIX: on_connect argument

--- query_message.py
+++ query_message.py
@@ -3,17 +3,17 @@ import paho.mqtt.client as mqtt
 # MQTT host information
 host = "mqtt.cloud.uiam.sk"  # replace with your host's IP or hostname
 port = 1883  # default MQTT port
-topic = "shellies/Shelly3EM-Main-Switchboard-C/emeter/0/dynamic_limits"  # topic to subscribe to
+# topic to subscribe to
+topic = "shellies/Shelly3EM-Main-Switchboard-C/emeter/0/dynamic_limits"
+
 
 # MQTT callback functions
-
-
-def on_connect(client, flags, rc):
+def on_connect(self, client, flags, rc):
     print("Connected with result code " + str(rc))
-    client.subscribe(topic)
+    self.subscribe(topic)
 
 
-def on_message(client, msg):
+def on_message(self, client, msg):
     print(msg.payload)
     print("Received message: " + msg.payload.decode())
 
