6da60b91bf4f53282ec593b1debf847311cb29ed
marekwadinger <marekwadinger5506@gmail.com>
2023-08-01 17:14:29 +0200

    REFACTOR: flake8

--- functions/evaluate.py
+++ functions/evaluate.py
@@ -12,6 +12,8 @@ def progressive_val_predict(  # noqa: C901
         metrics: list[BinaryMetric],
         print_every: int = 0,
         protect_anomaly_detector: bool = False,
+        print_final: bool = True,
+        compute_limits: bool = False,
         **kwargs):
     system_anomaly = []
     change_point = []
@@ -42,7 +44,7 @@ def progressive_val_predict(  # noqa: C901
                 raise ValueError("Dataset must contain column 'anomaly' to "
                                  "use metrics.")
 
-        if hasattr(model, 'limit_one'):
+        if compute_limits and hasattr(model, 'limit_one'):
             thresh_high, thresh_low = model.limit_one(x)
             ths.append(thresh_high)
             tls.append(thresh_low)
@@ -67,15 +69,16 @@ def progressive_val_predict(  # noqa: C901
             model = model.learn_one(x)
 
     end = time.time()
-    print(f"Avg. latency per sample: {(end - start)*1000/len(dataset)}ms")
-    if metrics is not None:
-        for metric in metrics:
-            print(metric)
+    if print_final:
+        print(f"Avg. latency per sample: {(end - start)*1000/len(dataset)}ms")
+        if metrics is not None:
+            for metric in metrics:
+                print(metric)
 
-    if hasattr(model, 'limit_one'):
+    if compute_limits and hasattr(model, 'limit_one'):
         return system_anomaly, change_point, ths, tls
     else:
-        return system_anomaly, change_point
+        return system_anomaly, change_point, None, None
 
 
 def print_stats(df, y_pred, change_point):
@@ -83,9 +86,10 @@ def print_stats(df, y_pred, change_point):
     res = pd.concat([df.anomaly, df_y_pred], axis=1)
     real = res[res['anomaly'] == 1]
     sum_ = sum(real.apply(lambda x: x['anomaly'] == x[0], axis=1))
+    len_real = len(real) if not len(real) == 0 else float('nan')
     print(f"{'Pred anomalous samples | events | proportion:':<55} "
           f"{sum(df_y_pred):<8} | {sum(df_y_pred.diff().dropna() == 1):<5} | "
           f"{sum(df_y_pred)/len(df_y_pred):.02%}\n"
           f"{'Pred changepoints:':<55} {sum(change_point)}\n"
           f"{'Found samples | events | proportion:':<55} "
-          f"{sum_:<8} | {' ':<5} | {sum_/len(real):.02%}")
+          f"{sum_:<8} | {' ':<5} | {sum_/len_real:.02%}")
