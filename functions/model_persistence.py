2351bc599d2e0e1824bceb55c84a306d44fb40be
marekwadinger <marekwadinger5506@gmail.com>
2023-10-31 16:07:05 +0100

    FIX: recovery model name

--- functions/model_persistence.py
+++ functions/model_persistence.py
@@ -9,10 +9,8 @@ from functions.utils import common_prefix
 
 def load_model(path: str, topics: list[str]):
     if path:
-        model_files = glob.glob(
-            os.path.join(
-                path, f"model_{common_prefix(topics)}_*.pkl")
-            )
+        model_name = f"model_{common_prefix(topics).replace('/', '_')}_*.pkl"
+        model_files = glob.glob(os.path.join(path, model_name))
         if model_files:
             model_files.sort(reverse=True)
             for latest_model in model_files:
@@ -29,10 +27,11 @@ def load_model(path: str, topics: list[str]):
 
 def save_model(path: str, topics: list[str], model):
     if path:
+        model_prefix = f"model_{common_prefix(topics).replace('/', '_')}"
         now = dt.datetime.utcnow().strftime("%Y%m%d-%H%M%S")
         if not os.path.exists(path):
             os.makedirs(path)
-        recovery_path = f"{path}/model_{common_prefix(topics)}_{now}.pkl"
+        recovery_path = f"{path}/{model_prefix}_{now}.pkl"
         with open(recovery_path, 'wb') as f:
             joblib.dump({"model": model, "topics": topics}, f)
             print(f"Model saved to {recovery_path}")
